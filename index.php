06141da201b6a7239f7d1f450e521fde9a0db396
岡安宏樹 <okayasuhiroki@okayasuhirokinoMacBook-Air.local>
2023-01-18 16:45:07 +0900

    first-commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<table>
+<?php
+for ($a =1; $a <=9; $a++) { ?>
+  <tr></tr>
+<?php for ($b =1; $b <=9; $b++) { ?>
+  <td><?php echo $a*$b ?></td>
+<?php } ?>
+<?php } ?>
+</table>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+$name = $_POST['name'];
+?>
+
+<p><?php echo $name; ?>さんご登録ありがとうございます</p>
